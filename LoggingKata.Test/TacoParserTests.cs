46b521b93c5aa3355410c68a545972d9c8660f34
Colbee Thrasher <colbeethr@gmail.com>
2026-01-31 16:33:55 -0600

    made INLINEDATA

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -21,6 +21,8 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("33.951387,-84.061032,Taco Bell Lawrenceville...",-84.061032)]
+        [InlineData("33.594359,-86.122022,Taco Bell Lincoln...",-86.122022)]
         //Add additional inline data. Refer to your CSV file.
         public void ShouldParseLongitude(string line, double expected)
         {
@@ -28,8 +30,7 @@ namespace LoggingKata.Test
             //       Note: "line" string represents input data we will Parse 
             //       to extract the Longitude.  
             //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
+            //       represents a TacoBell location ';];[;';
             //Arrange
             var tacoParser = new TacoParser();
 
@@ -42,6 +43,9 @@ namespace LoggingKata.Test
 
 
         //TODO: Create a test called ShouldParseLatitude
+        [Theory]
+        [InlineData("30.192338,-85.83407,Taco Bell Panama City Beach...", -85.83407)]
+        [InlineData("30.445296,-87.240548,Taco Bell Pensacola... ", -87.240548)]
 
         public void ShouldParseLatitude(string line, double expected)
         {
